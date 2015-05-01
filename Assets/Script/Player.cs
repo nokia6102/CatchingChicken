ebf3d3f4bb80777bb10695f2896284c9c936181d
nokia6102 <nokia6102@gmail.com>
2015-05-01 23:58:01 +0800

    Touch & bUseAccerlation control both.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour {
 	private bool bRight=true;
 	Animator anim;
 	public bool bUseAccerlation=true;
+	public bool bUseTouch=true;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +18,17 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (!bUseAccerlation) {
+		if (bUseTouch) {
 						if (Input.GetMouseButtonDown (0)) {
 								StartPos = Input.mousePosition;
 								bMouseDown = true;
+							
+								bUseAccerlation=false;
 						}
 
 						if (Input.GetMouseButtonUp (0)) {
 								bMouseDown = false;
+								bUseAccerlation=true;
 						}
 	
 						anim.SetBool ("bRun", false);
@@ -37,38 +41,42 @@ public class Player : MonoBehaviour {
 								scale.x *= -1.0f;
 								transform.localScale = scale;
 						}
-				} else
-				{
-						anim.SetBool ("bRun", false);
-						float dir = Input.acceleration.x;
-						dir = dir * Time.deltaTime;
-						float MoveSpeed = 40;
-						float len = dir * MoveSpeed;
-
- 
-						Vector3 p3 = new Vector3 (transform.position.x + len, transform.position.y, transform.position.z);
-			
-						float w = Camera.main.orthographicSize * Camera.main.aspect - bucket.renderer.bounds.size.x / 2;
-						p3.x = Mathf.Max (p3.x, -w);
-						p3.x = Mathf.Min (p3.x, w);
-						transform.position = p3;
-			
-						StartPos = EndPos;
-			
-						if (Mathf.Abs (len) > 0.01f) {
-								if (dir > 0)
+						else
+						{
+							if (bUseAccerlation) {
+								anim.SetBool ("bRun", false);
+								float dir = Input.acceleration.x;
+								dir = dir * Time.deltaTime;
+								float MoveSpeed = 40;
+								float len = dir * MoveSpeed;
+								
+								
+								Vector3 p3 = new Vector3 (transform.position.x + len, transform.position.y, transform.position.z);
+								
+								float w = Camera.main.orthographicSize * Camera.main.aspect - bucket.renderer.bounds.size.x / 2;
+								p3.x = Mathf.Max (p3.x, -w);
+								p3.x = Mathf.Min (p3.x, w);
+								transform.position = p3;
+								
+								StartPos = EndPos;
+								
+								if (Mathf.Abs (len) > 0.01f) {
+									if (dir > 0)
 										bRight = true;
-								if (dir < 0)
+									if (dir < 0)
 										bRight = false;
-								anim.SetBool ("bRun", true);
+									anim.SetBool ("bRun", true);
+								}
+								
+							//	Vector3 scale = transform.localScale;
+								if ((bRight && transform.localScale.x < 0) || (!bRight && transform.localScale.x > 0)) {  
+									scale.x *= -1.0f;
+									transform.localScale = scale;
+								}
+							}
 						}
-
-						Vector3 scale = transform.localScale;
-						if ((bRight && transform.localScale.x < 0) || (!bRight && transform.localScale.x > 0)) {  
-								scale.x *= -1.0f;
-								transform.localScale = scale;
-						}
-				}
+		}
+			
 	}
 
 	void DetectGuesture()
@@ -92,6 +100,7 @@ public class Player : MonoBehaviour {
 						if (p2.x < p1.x)
 								bRight = false;
 						anim.SetBool("bRun",true);
+	
 				}
 
 		}
